279c374b40aa2debed991176c88a7cbbbe7de005
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-04-20 04:00:17 +0530

    Pagination And User's Posts Categorisation

--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -29,7 +29,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 # localhost:5000/
 @app.route("/")
 def home():
-    POSTS = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    POSTS = Post.query.order_by(
+        Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=POSTS)
 
 
@@ -165,6 +167,16 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
 
 
 @app.route('/logout')
